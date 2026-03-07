dad303ce37b5b6ad0ce838fd3d9e41e8b0109062
ayoubmadani <ayoubbansalah123456@gmail.com>
2026-03-07 18:40:06 +0100

    Fix: store id and store url 3

--- src/constents/const..js
+++ src/constents/const..js
@@ -1,3 +1,3 @@
 // ✅ استخدام import.meta.env بدلاً من process.env
-export const baseURL = import.meta.env.VITE_STORE_URL;
-export const storeURL = import.meta.env.VITE_API_URL;
+export const baseURL = import.meta.env.VITE_API_URL;
+export const storeURL = import.meta.env.VITE_STORE_URL;
